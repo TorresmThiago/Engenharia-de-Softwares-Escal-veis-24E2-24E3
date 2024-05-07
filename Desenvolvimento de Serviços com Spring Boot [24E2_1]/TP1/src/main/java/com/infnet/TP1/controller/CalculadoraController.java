b1c10812bf97c47fa661fd8e3bb1e7aafa273697
Thiago Torres <11080794+TorresmThiago@users.noreply.github.com>
2024-05-06 22:09:46 -0300

    GET and POST mapped

--- Desenvolvimento de Serviços com Spring Boot [24E2_1]/TP1/src/main/java/com/infnet/TP1/controller/CalculadoraController.java
+++ Desenvolvimento de Serviços com Spring Boot [24E2_1]/TP1/src/main/java/com/infnet/TP1/controller/CalculadoraController.java
@@ -2,6 +2,8 @@ package com.infnet.TP1.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/calculadora")
 public class CalculadoraController {
@@ -10,18 +12,26 @@ public class CalculadoraController {
         return num1 + num2;
     }
 
-    @RequestMapping(value = "/adicao", method = { RequestMethod.GET, RequestMethod.POST })
+    @GetMapping(value = "/adicao")
     public double adicao(@RequestParam double num1, @RequestParam double num2) {
         return adicaoMethod(num1, num2);
+    }
+    @PostMapping(value = "/adicao")
+    public double adicao(@RequestBody Map<String, Double> requestParams) {
+        return adicaoMethod(requestParams.get("num1"), requestParams.get("num2"));
     }
 
     public double subtracaoMethod(double num1, double num2){
         return num1 - num2;
     }
 
-    @RequestMapping(value = "/subtracao", method = { RequestMethod.GET, RequestMethod.POST })
+    @GetMapping(value = "/subtracao")
     public double subtracao(@RequestParam double num1, @RequestParam double num2) {
         return subtracaoMethod(num1, num2);
+    }
+    @PostMapping(value = "/subtracao")
+    public double subtracao(@RequestBody Map<String, Double> requestParams) {
+        return subtracaoMethod(requestParams.get("num1"), requestParams.get("num2"));
     }
 
     public double divisaoMethod(double num1, double num2){
@@ -32,26 +42,38 @@ public class CalculadoraController {
         return num1 / num2;
     }
 
-    @RequestMapping(value = "/divisao", method = { RequestMethod.GET, RequestMethod.POST })
+    @GetMapping(value = "/divisao")
     public double divisao(@RequestParam double num1, @RequestParam double num2) {
         return divisaoMethod(num1, num2);
+    }
+    @PostMapping(value = "/divisao")
+    public double divisao(@RequestBody Map<String, Double> requestParams) {
+        return divisaoMethod(requestParams.get("num1"), requestParams.get("num2"));
     }
 
     public double multiplicacaoMethod(double num1, double num2){
         return num1 * num2;
     }
 
-    @RequestMapping(value = "/multiplicacao", method = { RequestMethod.GET, RequestMethod.POST })
+    @GetMapping(value = "/multiplicacao")
     public double multiplicacao(@RequestParam double num1, @RequestParam double num2) {
         return multiplicacaoMethod(num1, num2);
+    }
+    @PostMapping(value = "/multiplicacao")
+    public double multiplicacao(@RequestBody Map<String, Double> requestParams) {
+        return multiplicacaoMethod(requestParams.get("num1"), requestParams.get("num2"));
     }
 
     public double exponenciacaoMethod(double num1, double num2){
         return Math.pow(num1, num2);
     }
 
-    @RequestMapping(value = "/exponenciacao", method = { RequestMethod.GET, RequestMethod.POST })
+    @GetMapping(value = "/exponenciacao")
     public double exponenciacao(@RequestParam double num1, @RequestParam double num2) {
         return exponenciacaoMethod(num1, num2);
+    }
+    @PostMapping(value = "/exponenciacao")
+    public double exponenciacao(@RequestBody Map<String, Double> requestParams) {
+        return exponenciacaoMethod(requestParams.get("num1"), requestParams.get("num2"));
     }
 }
