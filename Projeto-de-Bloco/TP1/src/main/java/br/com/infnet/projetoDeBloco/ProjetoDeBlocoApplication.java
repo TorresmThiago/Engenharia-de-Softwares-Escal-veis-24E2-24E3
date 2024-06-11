77c2598d1d3b1b8ae4480344f516a4315211434f
Thiago Torres <11080794+TorresmThiago@users.noreply.github.com>
2024-06-11 17:04:16 -0300

    Rename project

--- Projeto-de-Bloco/TP1/src/main/java/br/com/infnet/projetoDeBloco/ProjetoDeBlocoApplication.java
+++ Projeto-de-Bloco/TP1/src/main/java/br/com/infnet/projetoDeBloco/ProjetoDeBlocoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Tp1Application {
+public class ProjetoDeBlocoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Tp1Application.class, args);
+		SpringApplication.run(ProjetoDeBlocoApplication.class, args);
 	}
 
 }
