13811692725e7d0179d6d561723eaa9393480527
Thiago Torres <11080794+TorresmThiago@users.noreply.github.com>
2024-08-26 23:09:39 -0300

    TP2

---
+++ Microsserviços e DevOps com Spring Boot e Spring Cloud [24E3_3]/TP2/tp2-app/src/test/java/br/infnet/edu/tp2_app/Tp2AppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.infnet.edu.tp2_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
