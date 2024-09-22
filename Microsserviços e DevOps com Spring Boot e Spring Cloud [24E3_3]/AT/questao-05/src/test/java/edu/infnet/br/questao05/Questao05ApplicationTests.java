c2e092481d41181e234782917ac33b64eb25dccc
Thiago Torres <11080794+TorresmThiago@users.noreply.github.com>
2024-09-22 20:22:40 -0300

    Questão 05

---
+++ Microsserviços e DevOps com Spring Boot e Spring Cloud [24E3_3]/AT/questao-05/src/test/java/edu/infnet/br/questao05/Questao05ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.infnet.br.questao05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Questao05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
