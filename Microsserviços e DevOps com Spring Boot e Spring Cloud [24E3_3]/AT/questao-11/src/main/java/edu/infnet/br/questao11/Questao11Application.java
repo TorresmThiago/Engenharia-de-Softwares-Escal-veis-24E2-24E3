0d42cf02cb041b861367f849b71b9df2b8496253
Thiago Torres <11080794+TorresmThiago@users.noreply.github.com>
2024-09-23 18:28:53 -0300

    questao 11

---
+++ Microsserviços e DevOps com Spring Boot e Spring Cloud [24E3_3]/AT/questao-11/src/main/java/edu/infnet/br/questao11/Questao11Application.java
@@ -0,0 +1,13 @@
+package edu.infnet.br.questao11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Questao11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Questao11Application.class, args);
+    }
+
+}
---
+++ Microsserviços e DevOps com Spring Boot e Spring Cloud [24E3_3]/AT/questao-11/src/test/java/edu/infnet/br/questao11/Questao11ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.infnet.br.questao11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Questao11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
